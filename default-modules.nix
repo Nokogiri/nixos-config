d77fe48925d6fc7cc331e19af04f65bb0315cbba
nokogiri <nokogiri@gefjon.org>
2022-06-14 21:34:48 +0000

    structuring?

--- default-modules.nix
+++ default-modules.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [ 
+    ./default.nix
     ./modules/security.nix
   ];
 }
---
+++ default.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, lib, ... }:
+
+{
+  nix = {
+    package =  pkgs.nixFlakes;
+    extraOptions = ''
+      experimentl-features = nix-command flakes
+    '';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 20d";
+    };
+  };
+  
+  nixpkgs.config.allowUnfree = true;
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
+    ];
+    extraLocaleSettings = {
+      LC_CTYPE = "en_US.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+      LC_COLLATE = "en_US.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_IDENTIFICATION= "en_US.UTF-8";
+    };
+  };
+
+  console.font = "Lat2-14";
+
+  time.timeZone = "Europe/Berlin";
+
+  
+}

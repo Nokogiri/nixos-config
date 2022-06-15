3197748cf98a6f0c859d6b765a16f82fa91af312
nokogiri <nokogiri@gefjon.org>
2022-06-15 09:38:19 +0000

    fonts.nix

--- default-modules.nix
+++ default-modules.nix
@@ -3,6 +3,7 @@
 {
   imports = [ 
     ./default.nix
+    ./modules/fonts.nix
     ./modules/security.nix
   ];
 }
---
+++ modules/fonts.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }: 
+
+{
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "FiraMono" "Meslo" "Mononoki" ]
+    ; })
+    corefonts
+    dejavu_fonts
+    unifont
+    noto-fonts
+    noto-fonts-emoji
+    noto-fonts-cjk-sans
+  ];
+
+  fonts = {
+    enableDefaultFonts = true;
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fontconfig = {
+      enable = true;
+      antialias = true;
+      defaultFonts = {
+        serif = [ "MesloLGSDZ Nerd Font" ];
+        sansSerif = [ "MesloLGSDZ Nerd Font" ];
+        monospace = [ "mononoki Nerd Font Mono" ];
+      };
+    };
+  };
+}

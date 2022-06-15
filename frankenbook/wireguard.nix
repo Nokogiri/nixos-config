0fa33c62e5c3912aa8430e7ba6c30c3134ce4f25
nokogiri <nokogiri@gefjon.org>
2022-06-15 13:19:09 +0000

    frankenbook/wireguard.nix

--- frankenbook/wireguard.nix
+++ frankenbook/wireguard.nix
@@ -16,15 +16,28 @@
       PrivateKeyFile = config.sops.secrets."wg/private".path;
       ListenPort = 51872;
     };
-    wireguardPeers = {
-      AllowedIPs = [
-        "10.200.200.0/24"
-        "::/1"
-      ];
-      Endpoint = "46.38.240.252:51871";
-      PersistentKeepalive = 25;
-      PresharedKeyFile = config.sops.secrets."wg/psk".path;
-      PublicKey = "IqYGE/5SPmMPEs0us6ZkH5RlePZ2KJDpGxBgjCBnQno=";
+    wireguardPeers = [{
+      wireguardPeerConfig = {
+        AllowedIPs = [
+          "10.200.200.0/24"
+          "::/1"
+        ];
+        Endpoint = "46.38.240.252:51871";
+        PersistentKeepalive = 25;
+        PresharedKeyFile = config.sops.secrets."wg/psk".path;
+        PublicKey = "IqYGE/5SPmMPEs0us6ZkH5RlePZ2KJDpGxBgjCBnQno=";
+      };
+    }];
+  };
+  systemd.network.networks."90-wireguard.network" = {
+    matchConfigName = "wg0";
+    address = [ "10.200.200.2/24" ];
+    dns = [ "10.200.200.1" ];
+    routes = {
+      routeConfig = {
+        Destination = "10.200.200.0/24";
+        Scope = "link";
+      };
     };
   };
 }

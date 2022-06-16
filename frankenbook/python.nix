9f9f416c21cba301167d91071a638c2ce7d4b607
nokogiri <nokogiri@gefjon.org>
2022-06-16 21:03:19 +0000

    frankenbook/python.nix

--- frankenbook/python.nix
+++ frankenbook/python.nix
@@ -3,11 +3,12 @@
 {
   environment.systemPackages = with pkgs; [
     (let
-      my-pthon-packages = python-packages: with python-packages; [
+      my-python-packages = python-packages: with python-packages; [
         requests
       ];
       python-with-my-packages = python3.withPackages my-python-packages;
     in
+    python-with-my-packages)
   ];
 }
 

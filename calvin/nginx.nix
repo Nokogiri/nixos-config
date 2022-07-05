47eab73f2c3302a4401c0b17111918c1537860f5
nokogiri <nokogiri@gefjon.org>
2022-07-05 18:02:35 +0000

    flake update

--- calvin/nginx.nix
+++ calvin/nginx.nix
@@ -78,5 +78,16 @@
           ;
       };
     };
+    virtualHosts."cache.fishoeder.net" = {
+      useACMEHost = "fishoeder.net";
+      forceSSL = true;
+      serverAliases = [ "binarycache" ];
+      locations."/".extraConfig = ''
+        proxy_pass http://localhost:${toString config.services.nix-serve.port};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      '';
+    };
   };
 }
--- calvin/services.nix
+++ calvin/services.nix
@@ -5,6 +5,10 @@
     openssh.enable = true;
     qemuGuest.enable = true;
     jellyfin.enable = true;
+    nix-serve = {
+      enable = true;
+      secretKeyFile = "/var/cache-priv-key.pem";
+    };
   };
 
 

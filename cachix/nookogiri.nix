a2d0ca84c689f18112cd2ce4558572926608ff32
nokogiri <nokogiri@gefjon.org>
2022-07-05 21:42:28 +0000

    cahcix

---
+++ cachix/nookogiri.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://nookogiri.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "nookogiri.cachix.org-1:tKY3HBn6Xhuqys5EcQRl1u+sKualOXFzCs9Q6qEXR+o="
+    ];
+  };
+}

6b9b1fa3fd794b23f15e8fbdd023b510fa8818a7
nokogiri <nokogiri@gefjon.org>
2022-07-13 18:25:37 +0000

    invidious vhost

--- calvin/nginx.nix
+++ calvin/nginx.nix
@@ -89,17 +89,17 @@
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
       '';
     };
-    virtualHost."invid.fishoeder.net" = {
+    virtualHosts."invid.fishoeder.net" = {
       useACMEHost = "fishoeder.net";
       forceSSL = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:3000/";
-        extraConfig = 
-        "proxy_set_header X-Forwarded-For $remote_addr;" +
-        "proxy_set_header Host $host;" +
-        "proxy_http_version 1.1;" +
-        "proxy_set_header Connection "";"
-        ;
+        extraConfig = '' 
+        proxy_set_header X-Forwarded-For $remote_addr;
+        proxy_set_header Host $host;
+        proxy_http_version 1.1;
+        proxy_set_header Connection "";
+        '';
       };
     };
   };

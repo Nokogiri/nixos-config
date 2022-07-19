58399661377d709253d272213a60fcfe04a1ef60
nokogiri <nokogiri@gefjon.org>
2022-07-19 22:48:40 +0200

    testing

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -10,13 +10,68 @@
     ''; # Will automatically open sway when logged into tty1
 
     sessionVariables = {
-      XDG_DATA_DIRS = "${pkgs.gnome-desktop-schemas}/share/gsettings/${pkgs.gnome-desktop-schemas.name}:$XDG_DATA_DIRS";
+      XDG_DATA_DIRS = "${pkgs.gsettings-desktop-schemas}/share/gsettings/${pkgs.gsettings-desktop-schemas.name}:$XDG_DATA_DIRS";
     };
   };
 
   programs.hyprland = {
     enable = true;
     extraPackages = with pkgs; [
+      (pkgs.writeTextFile {
+        name = "wayland_env";
+        destination = "/bin/wayland_env";
+        executable = true;
+        text =
+          let
+            schema = pkgs.gsettings-desktop-schemas;
+            datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+          in
+          ''
+            #
+            # GTK environment
+            #
+
+            #export GDK_BACKEND=wayland # May cause problems with some xorg applications
+            export TDESKTOP_DISABLE_GTK_INTEGRATION=1
+            export CLUTTER_BACKEND=wayland
+            export BEMENU_BACKEND=wayland
+
+            # Firefox
+            export MOZ_ENABLE_WAYLAND=1
+
+            #
+            # Qt environment
+            #
+            export QT_QPA_PLATFORM=wayland-egl #error with apps xcb
+            export QT_WAYLAND_FORCE_DPI=physical
+            export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+            #
+            # Elementary environment
+            #
+            export ELM_DISPLAY=wl
+            export ECORE_EVAS_ENGINE=wayland_egl
+            export ELM_ENGINE=wayland_egl
+            export ELM_ACCEL=opengl
+            # export ELM_SCALE=1
+
+            #
+            # SDL environment
+            #
+            export SDL_VIDEODRIVER=wayland
+
+            #
+            # Java environment
+            #
+            export _JAVA_AWT_WM_NONREPARENTING=1
+
+            export NO_AT_BRIDGE=1
+            export WINIT_UNIX_BACKEND=wayland
+            export DBUS_SESSION_BUS_ADDRESS
+            export DBUS_SESSION_BUS_PID
+            export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+          '';
+      })
       cliphist
       firefox-esr-wayland
       fluent-gtk-theme

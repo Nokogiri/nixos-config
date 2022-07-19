593dc5a2f4249dacce33adf39f8c8fe110f542e5
nokogiri <nokogiri@gefjon.org>
2022-07-19 22:37:32 +0200

    testing

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -10,7 +10,7 @@
     ''; # Will automatically open sway when logged into tty1
 
     sessionVariables = {
-      "XDG_DATA_DIRS=${pkgs.gnome-desktop-schemas}/share/gsettings/${pkgs.gnome-desktop-schemas.name}:$XDG_DATA_DIRS"
+      "XDG_DATA_DIRS=${pkgs.gnome-desktop-schemas}/share/gsettings/${pkgs.gnome-desktop-schemas.name}:$XDG_DATA_DIRS";
     };
   };
 

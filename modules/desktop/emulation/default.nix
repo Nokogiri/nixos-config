bf5ab903ce0eef1a706edb02b176cd3c726cdcdd
nokogiri <nokogiri@gefjon.org>
2022-07-20 16:35:59 +0200

    emulation stuff

---
+++ modules/desktop/emulation/default.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, lib, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    gamemode
+    ppsspp-sdl
+    retroarch
+    libretro.mgba
+    libretro.bsnes
+    libretro.bsnes-hd
+    libretro.snes9x
+    #libretro.ppsspp
+    libretro.flycast
+    libretro.melonds
+    libretro.gambatte
+    libretro.genesis-plus-gx
+    libretro.beetle-psx
+    libretro.beetle-psx-hw
+    libretro.beetle-pce-fast
+    libretro.beetle-supergrafx
+    libretro.mupen64plus
+    libretro.parallel-n64
+  ];
+}
+

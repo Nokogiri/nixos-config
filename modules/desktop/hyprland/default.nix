67756f401ef62252a81467d97059807b69dbe883
nokogiri <nokogiri@gefjon.org>
2022-07-22 22:29:53 +0200

    flake update

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -138,8 +138,8 @@
 
       text = ''
         dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
-        systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-        systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+        #systemctl --user stop pipewire xdg-desktop-portal
+        #systemctl --user start pipewire xdg-desktop-portal
       '';
     })
   ];

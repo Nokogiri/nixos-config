b33583d1918ebccbc5b317c192ecd97351a01065
nokogiri <nokogiri@gefjon.org>
2022-07-26 16:33:12 +0200

    expanding home-manager

--- hosts/frankenbook/home.nix
+++ hosts/frankenbook/home.nix
@@ -22,19 +22,34 @@
   home = {
     packages = with pkgs;
       [
-        #Apps
         firefox-wayland
         gimp
-        inkscape
         gtk-pipe-viewer
-        #pipe-viewer
-        zathura
         imv
+        inkscape
         mpv
         ncspot
+        zathura
 
         #Games
-
+        gamemode
+        ppsspp-sdl
+        retroarch
+        libretro.mgba
+        libretro.bsnes
+        libretro.bsnes-hd
+        libretro.snes9x
+        #libretro.ppsspp
+        libretro.flycast
+        libretro.melonds
+        libretro.gambatte
+        libretro.genesis-plus-gx
+        libretro.beetle-psx
+        libretro.beetle-psx-hw
+        libretro.beetle-pce-fast
+        libretro.beetle-supergrafx
+        libretro.mupen64plus
+        libretro.parallel-n64
         (polymc.override {
           msaClientID = "0b5d07b7-fcd2-4f81-901a-7596869a0cee";
         })

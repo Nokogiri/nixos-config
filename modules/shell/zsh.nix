58b86a5b68ae5fe37610e0897a5437ab1078efb9
nokogiri <nokogiri@gefjon.org>
2022-07-27 22:43:00 +0200

    expanding home-manager - zsh.nix

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -7,6 +7,10 @@
       initExtra = ''
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+        source ~/.p10k.zsh
+        if [[ -r "$\{XDG_CACHE_HOME\}/p10k-instant-prompt-$\{(%):-%n\}.zsh" ]]; then
+          source "$\{XDG_CACHE_HOME/p10k-instant-prompt-$\{(%):-%n\}.zsh"
+        fi
       '';
       oh-my-zsh = {
         enable = true;
@@ -22,6 +26,15 @@
             repo = "zsh-autosuggestions";
             rev = "v0.4.0";
             sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+          };
+        }
+        {
+          name = "fzf-tab";
+          src = pkgs.fetchFromGitHub {
+            owner = "Aloxaf";
+            repo = "fzf-tab";
+            rev = "master";
+            sha256 = "";
           };
         }
       ];

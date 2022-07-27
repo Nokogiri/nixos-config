f99b1367405ddc37e597335803fce3c7f868f4cd
nokogiri <nokogiri@gefjon.org>
2022-07-27 23:31:15 +0200

    expanding home-manager - zsh.nix

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,19 +1,33 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   programs = {
     zsh = {
       dotDir = ".config/zsh";
       enable = true;
+      enableAutosuggestions = true;
       enableCompletion = true;
       enableSyntaxHighlighting = true;
+      initExtraFirst = ''
+        if [[ -r "$\{XDG_CACHE_HOME\}/p10k-instant-prompt-$\{(%):-%n\}.zsh" ]]; then
+          source "$\{XDG_CACHE_HOME/p10k-instant-prompt-$\{(%):-%n\}.zsh"
+        fi
+        export FZF_PREVIEW_ADVANCED=true
+      '';
       initExtra = ''
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
         source ~/.p10k.zsh
-        if [[ -r "$\{XDG_CACHE_HOME\}/p10k-instant-prompt-$\{(%):-%n\}.zsh" ]]; then
-          source "$\{XDG_CACHE_HOME/p10k-instant-prompt-$\{(%):-%n\}.zsh"
-        fi
       '';
+      history = {
+        expireDuplicatesFirst = true;
+        extended = true;
+        ignoreDups = true;
+        ignoreSpace = true;
+        path = "${config.xdg.configHome}/zsh/zsh_history";
+        save = 100000;
+        share = true;
+        size = 100000;
+      };
       oh-my-zsh = {
         enable = true;
         plugins = [
@@ -21,15 +35,15 @@
         ];
       };
       plugins = [
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-autosuggestions";
-            rev = "v0.7.0";
-            sha256 = "KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
-          };
-        }
+        #{
+        #  name = "zsh-autosuggestions";
+        #  src = pkgs.fetchFromGitHub {
+        #    owner = "zsh-users";
+        #    repo = "zsh-autosuggestions";
+        #    rev = "v0.7.0";
+        #    sha256 = "KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+        #  };
+        #}
         {
           name = "fzf-tab";
           src = pkgs.fetchFromGitHub {

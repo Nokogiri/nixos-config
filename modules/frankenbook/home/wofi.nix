67410a3feed491b382ffff011ce69ba4ca6fee15
nokogiri <nokogiri@gefjon.org>
2022-08-03 18:12:12 +0200

    misc

--- modules/frankenbook/home/wofi.nix
+++ modules/frankenbook/home/wofi.nix
@@ -1,9 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  #xdg.configFile."wofi/config".source = ./config;
   xdg.configFile."wofi/style.css".source = ./config/wofi/style.css;
-
   xdg.configFile."wofi/config".text = ''
     width=965
     height=540

9c42ad536cb34a262b3f729a3e7d837730b74916
nokogiri <nokogiri@gefjon.org>
2022-08-04 21:07:51 +0200

    up

--- modules/frankenbook/home/vscode.nix
+++ modules/frankenbook/home/vscode.nix
@@ -4,8 +4,16 @@
     enable = true;
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
+      bodil.file-browser
       jnoortheen.nix-ide
       yzhang.markdown-all-in-one
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "Theme-TomorrowKit";
+        publisher = "ms-vscode";
+        version = "0.1.4";
+        sha256 = "qakwJWak+IrIeeVcMDWV/fLPx5M8LQGCyhVt4TS/Lmc=";
+      }
     ];
   };
 }

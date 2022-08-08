600b8025277256cd0104b695c3fa1950e856b5de
nokogiri <nokogiri@gefjon.org>
2022-08-08 19:25:33 +0200

    wallpaper

---
+++ modules/frankenbook/home/config/wallpaper/default.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+{
+  xdg.configFile."wallpaper/wallpaper_01.png".source = ./wallpaper_01.png;
+  xdg.configFile."wallpaper/wallpaper_02.png".source = ./wallpaper_02.png;
+  xdg.configFile."wallpaper/wallpaper_03.png".source = ./wallpaper_03.png;
+  xdg.configFile."wallpaper/wallpaper_04.png".source = ./wallpaper_04.png;
+  xdg.configFile."wallpaper/wallpaper_05.png".source = ./wallpaper_05.png;
+  xdg.configFile."wallpaper/wallpaper_06.png".source = ./wallpaper_06.png;
+  xdg.configFile."wallpaper/wallpaper_07.png".source = ./wallpaper_07.png;
+  xdg.configFile."wallpaper/wallpaper_08.png".source = ./wallpaper_08.png;
+  xdg.configFile."wallpaper/wallpaper_09.png".source = ./wallpaper_09.png;
+  xdg.configFile."wallpaper/wallpaper_10.png".source = ./wallpaper_10.png;
+  xdg.configFile."wallpaper/wallpaper_11.png".source = ./wallpaper_11.png;
+  xdg.configFile."wallpaper/wallpaper_12.png".source = ./wallpaper_12.png;
+  xdg.configFile."wallpaper/wallpaper_13.png".source = ./wallpaper_13.png;
+  xdg.configFile."wallpaper/wallpaper_14.png".source = ./wallpaper_14.png;
+}

4dab2c5070d8fc7577b912b8585bbc9646e01d69
nokogiri <nokogiri@gefjon.org>
2022-11-17 21:28:32 +0100

    cleansing

--- modules/frankenbook/apps/default.nix
+++ modules/frankenbook/apps/default.nix
@@ -1,29 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      mpv = super.mpv.override {
-        scripts = [
-          self.mpvScripts.mpris
-        ];
-      };
-    })
-  ];
-
   environment.systemPackages = with pkgs; [
     brightnessctl
-    blueman
-    glib.bin
-    hunspell
     iwd
     ldns
-    pkg-config
     sshfs-fuse
     udisks
     udiskie
     yt-dlp
-    xdg_utils
   ];
 
 
--- modules/frankenbook/services/default.nix
+++ modules/frankenbook/services/default.nix
@@ -4,22 +4,14 @@
   services = {
 
     avahi.enable = true;
-    clight = {
-      enable = false;
-      temperature = {
-        day = 6500;
-        night = 4200;
-      };
-    };
     dbus.enable = true;
-    gnome.gnome-keyring.enable = false;
     logind.lidSwitch = "ignore";
     mbpfan = {
       enable = true;
       settings.general = {
         low_temp = 55;
         high_temp = 70;
-        max_temp = 84;
+        max_temp = 89;
         polling_interval = 2;
         min_fan1_speed = 2000;
       };
@@ -36,18 +28,5 @@
     tlp.enable = false;
     udisks2.enable = true;
 
-  };
-
-  virtualisation = {
-    docker = {
-      enable = true;
-      storageDriver = "btrfs";
-    };
-
-    libvirtd = {
-      enable = false;
-      onBoot = "ignore";
-      onShutdown = "shutdown";
-    };
   };
 }

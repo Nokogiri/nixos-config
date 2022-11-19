a14ee053dadde8e289ff79e546cfc3b677034083
nokogiri <nokogiri@gefjon.org>
2022-11-19 17:05:55 +0100

    rclone user service

--- modules/frankenbook/user-services/default.nix
+++ modules/frankenbook/user-services/default.nix
@@ -12,13 +12,16 @@
       serviceConfig = {
         Type = "notify";
         ExecStart = ''${pkgs.rclone}/bin/rclone mount \
-          --config=/var/lib/conf/rclone.conf \
-          --vfs-cache-mode writes \
-          --vfs-cache-max-size 100M \
+          --config=%h/.config/rclone/rclone.conf \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 256M \
           --log-level INFO \
           --log-file /tmp/rclone-%i.log \
           --umask 022 \
           --allow-other \
+          --allow-non-empty \
+          --dir-cache-time 48h \
+          --poll-interval 5m \
           nokogiri: /media/cloud/nokogiri
           '';
           ExecStop = "/run/wrappers/bin/fusermount -u /media/cloud/nokogiri";
@@ -36,13 +39,16 @@
       serviceConfig = {
         Type = "notify";
         ExecStart = ''${pkgs.rclone}/bin/rclone mount \
-          --config=/var/lib/conf/rclone.conf \
-          --vfs-cache-mode writes \
-          --vfs-cache-max-size 100M \
+          --config=%h/.config/rclone/rclone.conf \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 256M \
           --log-level INFO \
           --log-file /tmp/rclone-%i.log \
           --umask 022 \
           --allow-other \
+          --allow-non-empty \
+          --dir-cache-time 48h \
+          --poll-interval 5m \
           lyria: /media/cloud/lyria
           '';
           ExecStop = "/run/wrappers/bin/fusermount -u /media/cloud/lyria";
@@ -60,13 +66,16 @@
       serviceConfig = {
         Type = "notify";
         ExecStart = ''${pkgs.rclone}/bin/rclone mount \
-          --config=/var/lib/conf/rclone.conf \
-          --vfs-cache-mode writes \
-          --vfs-cache-max-size 100M \
+          --config=%h/.config/rclone/rclone.conf \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 256M \
           --log-level INFO \
           --log-file /tmp/rclone-%i.log \
           --umask 022 \
           --allow-other \
+          --allow-non-empty \
+          --dir-cache-time 48h \
+          --poll-interval 5m \
           guntheria: /media/cloud/guntheria
           '';
           ExecStop = "/run/wrappers/bin/fusermount -u /media/cloud/guntheria";
@@ -84,16 +93,71 @@
       serviceConfig = {
         Type = "notify";
         ExecStart = ''${pkgs.rclone}/bin/rclone mount \
-          --config=/var/lib/conf/rclone.conf \
-          --vfs-cache-mode writes \
-          --vfs-cache-max-size 100M \
+          --config=%h/.config/rclone/rclone.conf \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 256M \
           --log-level INFO \
           --log-file /tmp/rclone-%i.log \
           --umask 022 \
           --allow-other \
+          --allow-non-empty \
+          --dir-cache-time 48h \
+          --poll-interval 5m \
           markus: /media/cloud/markus
           '';
           ExecStop = "/run/wrappers/bin/fusermount -u /media/cloud/markus";
+          Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+      };
+    };
+    rclone-kenny = {
+      enable = true;
+      description = "rclone: Remote FUSE filesystem for cloud storage config kenny";
+      documentation = ["man:rclone(1)"];
+      after = ["network-online.target"];
+      wants = ["network-online.target"];
+      wantedBy = ["default.target"];
+      serviceConfig = {
+        Type = "notify";
+        ExecStart = ''${pkgs.rclone}/bin/rclone mount \
+          --config=%h/.config/rclone/rclone.conf \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 256M \
+          --log-level INFO \
+          --log-file /tmp/rclone-%i.log \
+          --umask 022 \
+          --allow-other \
+          --allow-non-empty \
+          --dir-cache-time 48h \
+          --poll-interval 5m \
+          kenny: /media/cloud/kenny
+          '';
+          ExecStop = "/run/wrappers/bin/fusermount -u /media/cloud/kenny";
+          Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+      };
+    };
+    rclone-hildi = {
+      enable = true;
+      description = "rclone: Remote FUSE filesystem for cloud storage config hildi";
+      documentation = ["man:rclone(1)"];
+      after = ["network-online.target"];
+      wants = ["network-online.target"];
+      wantedBy = ["default.target"];
+      serviceConfig = {
+        Type = "notify";
+        ExecStart = ''${pkgs.rclone}/bin/rclone mount \
+          --config=%h/.config/rclone/rclone.conf \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 256M \
+          --log-level INFO \
+          --log-file /tmp/rclone-%i.log \
+          --umask 022 \
+          --allow-other \
+          --allow-non-empty \
+          --dir-cache-time 48h \
+          --poll-interval 5m \
+          hildi: /media/cloud/hildi
+          '';
+          ExecStop = "/run/wrappers/bin/fusermount -u /media/cloud/hildi";
           Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
       };
     };

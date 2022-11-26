553e3f107ad6804f4ead5e6524431675bdb352fd
nokogiri <nokogiri@gefjon.org>
2022-11-27 00:21:44 +0100

    mowteng + too much to tell

--- home/mowteng/config/wallpaper/default.nix
+++ home/mowteng/config/wallpaper/default.nix
@@ -1,20 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg.configFile."wallpaper/wallpaper_01.png".source = ./wallpaper_01.png;
-  xdg.configFile."wallpaper/wallpaper_02.png".source = ./wallpaper_02.png;
-  xdg.configFile."wallpaper/wallpaper_03.png".source = ./wallpaper_03.png;
-  xdg.configFile."wallpaper/wallpaper_04.png".source = ./wallpaper_04.png;
-  xdg.configFile."wallpaper/wallpaper_05.png".source = ./wallpaper_05.png;
-  xdg.configFile."wallpaper/wallpaper_06.png".source = ./wallpaper_06.png;
-  xdg.configFile."wallpaper/wallpaper_07.png".source = ./wallpaper_07.png;
-  xdg.configFile."wallpaper/wallpaper_08.png".source = ./wallpaper_08.png;
-  xdg.configFile."wallpaper/wallpaper_09.png".source = ./wallpaper_09.png;
-  xdg.configFile."wallpaper/wallpaper_10.png".source = ./wallpaper_10.png;
-  xdg.configFile."wallpaper/wallpaper_11.png".source = ./wallpaper_11.png;
-  xdg.configFile."wallpaper/wallpaper_12.png".source = ./wallpaper_12.png;
-  xdg.configFile."wallpaper/wallpaper_13.png".source = ./wallpaper_13.png;
-  xdg.configFile."wallpaper/wallpaper_14.png".source = ./wallpaper_14.png;
-  xdg.configFile."wallpaper/wallpaper_15.png".source = ./wallpaper_15.png;
-  xdg.configFile."wallpaper/wallpaper_16.png".source = ./wallpaper_16.png;
+  xdg.configFile."wallpaper/wall-01.webp".source = ./wall-01.webp;
+  xdg.configFile."wallpaper/wall-02.webp".source = ./wall-02.webp;
+  xdg.configFile."wallpaper/wall-03.webp".source = ./wall-03.webp;
+  #xdg.configFile."wallpaper/wall-04.webp".source = ./wall-04.webp;
+  #xdg.configFile."wallpaper/wall-05.webp".source = ./wall-05.webp;
+  #xdg.configFile."wallpaper/wall-06.webp".source = ./wall-06.webp;
+  #xdg.configFile."wallpaper/wall-07.webp".source = ./wall-07.webp;
+  #xdg.configFile."wallpaper/wall-08.webp".source = ./wall-08.webp;
+  #xdg.configFile."wallpaper/wall-09.webp".source = ./wall-09.webp;
+  #xdg.configFile."wallpaper/wall-10.webp".source = ./wall-10.webp;
+  #xdg.configFile."wallpaper/wall-11.webp".source = ./wall-11.webp;
+  #xdg.configFile."wallpaper/wall-12.webp".source = ./wall-12.webp;
+  #xdg.configFile."wallpaper/wall-13.webp".source = ./wall-13.webp;
+  #xdg.configFile."wallpaper/wall-14.webp".source = ./wall-14.webp;
+  #xdg.configFile."wallpaper/wall-15.webp".source = ./wall-15.webp;
+  #xdg.configFile."wallpaper/wall-16.webp".source = ./wall-16.webp;
 }

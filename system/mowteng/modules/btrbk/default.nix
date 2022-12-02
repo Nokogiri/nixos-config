d685c20a7476bbe81ef398741b8430b1c6e2302a
nokogiri <nokogiri@gefjon.org>
2022-12-02 23:53:58 +0100

    mowteng >-> btrbk

--- system/mowteng/modules/btrbk/default.nix
+++ system/mowteng/modules/btrbk/default.nix
@@ -25,6 +25,36 @@
           };
         };
       };
+      "homeassistant" = {
+        settings = {
+          ssh_identity = "/home/nokogiri/.ssh/id_ed25519";
+          ssh_user = "nokogiri";
+          backend_remote = "btrfs-progs-sudo";
+          stream_buffer = "512m";
+          stream_compress = "zstd";
+          stream_compress_level = "9";
+          stream_compress_threads = "4";
+          snapshot_create = "ondemand";
+          incremental = "yes";
+          snapshot_preserve_min = "3d";
+          snapshot_preserve = "14d";
+          target_preserve_min = "no";
+          target_preserve = "20d 10w *m";
+
+          volume = {
+            "ssh://homeassistant.home.arpa/data/butter" = {
+              snapshot_dir = "@snapshots";
+              target = "/media/btrbk/snapshots_homeassistant";
+              subvolume = {
+                "@nixos" = { };
+                "@nixos/srv" = { };
+                "@nixos/var" = { };
+                "@home" = { };
+              };
+            };
+          };
+        };
+      };
     };
   };
 }

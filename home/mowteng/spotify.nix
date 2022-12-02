92e4ce8438352e5e1c0cfa23087ad3b76441a43a
nokogiri <nokogiri@gefjon.org>
2022-12-02 22:07:05 +0100

    spicetify-nix

--- home/mowteng/spotify.nix
+++ home/mowteng/spotify.nix
@@ -1,13 +1,16 @@
-{ pkgs, lib, spicetify-nix, ... }: {
+{ pkgs, lib, spicetify-nix, ... }: 
+{
 
-  #imports = [ spicetify-nix.homeManagerModule ];
   programs.spicetify = {
     enable = true;
     theme = "catppuccin-frappe";
     # OR 
     # theme = spicetify-nix.pkgSets.${pkgs.system}.themes.catppuccin-mocha;
     colorScheme = "teal";
-
-    enabledExtensions = [ "fullAppDisplay.js" "shuffle+.js" "hidePodcasts.js" ];
+  
+    enabledCustomApps = [
+      "lyrics-plus"
+    ];
+    enabledExtensions = [ "shuffle+.js" "hidePodcasts.js" ];
   };
 }

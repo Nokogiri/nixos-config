c63bdf84588d050a038d7d357815e604cafea110
nokogiri <nokogiri@gefjon.org>
2022-12-12 14:34:41 +0100

    avahi for homeassistant

---
+++ system/homeassistant/modules/avahi/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, lib, config, ... }: {
+  services.avahi = {
+    enable = true;
+    hostName = config.networking.hostName;
+    nssmdns = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      hinfo = true;
+      userServices = true;
+    };
+  };
+}
